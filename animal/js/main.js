44b30fcbc8e7f6e2ca8502aaa0a60eda88fe0ee0
jd01451 <jdp01451@gmail.com>
2025-05-29 16:23:52 +0900

    animal

--- animal/js/main.js
+++ animal/js/main.js
@@ -66,7 +66,7 @@ $(document).ready(function(){
             $(this).addClass('over')
         }
     })
-    $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseleave ', function(){
+    $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseleave', function(){
         //console.log('아웃')
         $(this).removeClass('over')
     })
@@ -176,4 +176,52 @@ $(document).ready(function(){
     });
     /****************  분양 swiper 종료**********************/
 	
+    /***************** 후기 swiper 시작 *********************/
+    const review_swiper = new Swiper('.review .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            1300: {    /* 640px 이상일때 적용 */
+            slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+            spaceBetween: 24,
+            },
+            1024: {    /* 640px 이상일때 적용 */
+                slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+            768: {    /* 640px 이상일때 적용 */
+                slidesPerView: 2,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+        
+        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        // autoplay: {  /* 팝업 자동 실행 */
+        //     delay: 2500,
+        //     disableOnInteraction: true,
+        // },
+        navigation: {
+            nextEl: '.review .list .btn_next',
+            prevEl: '.review .list .btn_prev',
+        },
+        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+            el: '.swiper-pagination', /* 해당 요소의 class명 */
+            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+            type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+        },
+    });
+    // review_swiper.autoplay.stop();  /* 일시정지 기능 */
+    // review_swiper.autoplay.start();  /* 재생 기능 */
+
+    /* footer top버튼 :: 시작 */
+
+    $('footer .top span').on('click', function(){
+        console.log('클릭')
+        $('html, body').animate({ /* 이동할 오브젝트 */
+            scrollTop: 0 /* 스크롤 값 */
+        }, 500) /* 애니메이션 시간 */
+    })
+
+    /* footer top버튼 :: 종료 */
 })//document.ready
