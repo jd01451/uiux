44d0cb444c982668d06c7e8120a19b1cb095ffa5
jd01451 <jdp01451@gmail.com>
2025-06-03 17:59:55 +0900

    d

---
+++ kumho/js/common.js
@@ -0,0 +1,85 @@
+/* header, footer 공통요소에 들어가는 javascript/jquery */
+/*
+* pc버전, 모바일 버전 구분
+* 스크롤된값 계산
+*  
+* 스크롤을 내리면 header에 fixed 클래스 주기
+* 메뉴에 마우스를 올리면 header에 menu_over 클래스 추가
+* 메뉴를 오버한 li에 over 클래스 추가
+* 
+* 스크롤을 내릴때는 gnb_up 클래스 추가
+* 스크롤을 올릴때는 gnb_up 클래스 삭제
+* ===> 이전에 스크롤값과 현재 스크롤 값을 비교해서
+*      현재 값이 더크면 내려가는 중 ( 100 --> 200 )
+*      현재 값이 작으면 올라가는 중 ( 200 --> 100 )
+*/
+
+let device_status //pc인지 모바일 구분하는 값
+let scrolling //브라우저가 스크롤 된 값
+let scroll_prev //이전에 스크롤 된값
+let window_w //브라우저의 넓이 값
+let mobile_size = 1024 // 모바일로 변경되는 사이트
+
+$(window).scroll(function(){//브라우저가 스크롤 될떄마다 1번 실행
+    //console.log('브라우저가 스크롤 됨')
+    scroll_chk()
+})
+$(window).resize(function(){
+    //console.log('리사이즈')
+    resize_chk()
+})
+$(document).ready(function(){ //html이 로딩되고 로드1번
+    //console.log('로딩됐다')
+    resize_chk() //함수 실행
+    scroll_chk()
+
+    $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseenter', function(){
+        if(device_status == 'pc'){
+            //console.log('오버')
+            $('header').addClass('menu_over')
+            $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('over')
+            $(this).addClass('over')
+        }
+    })
+    $('header').on('mouseleave', function(){
+        $('header').removeClass('menu_over')
+        $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('over')
+    })
+
+    /* 모바일메뉴 열고닫ㄱ기 */
+    $('header .gnb .gnb_open').on('click', function(){
+        $('header').addClass('menu_open')
+    })
+    $('header .gnb .gnb_close').on('click', function(){
+        $('header').removeClass('menu_open')
+    })
+})
+
+//함수의 선언
+function resize_chk(){
+    window_w = $(window).width()
+    console.log(window_w)
+    if(window_w > mobile_size){
+        device_status = 'pc'
+    }else{ //같거나 작으면
+        device_status = 'mobile'
+    }
+    //console.log(device_status)
+}
+function scroll_chk(){
+    scroll_prev = scrolling //스크롤링 값을 scroll_prev에 할당
+    scrolling = $(window).scrollTop()
+    console.log(scroll_prev, scrolling)
+    if(scrolling > 0){
+        $('header').addClass('fixed')
+        if(scrolling > scroll_prev){
+            $('header').addClass('gnb_up')
+            //console.log('내려가는중')
+        }else{
+            $('header').removeClass('gnb_up')
+            //console.log('올라가는중')
+        }
+    }else{
+        $('header').removeClass('fixed')
+    }
+}
